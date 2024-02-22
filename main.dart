3623cd4786df74255eb4ed5f0b3211867a5badd0
chalolowa <wangwelocha@students.uonbi.ac.ke>
2024-02-22 20:04:07 +0300

    On branch main
     Introduction to dart programming

---
+++ main.dart
@@ -0,0 +1,15 @@
+void main() {
+  String name = "locha";
+  int age = 1000;
+  bool isSingle = true;
+
+  //Don't mind this just thinking in between
+
+  print(name);
+  print(age);
+  print(isSingle);
+  print(
+      "Hello there!! My name is $name and i'm $int years old. Also very $isSingle");
+      
+  /*Oh! its been fun bit short but its introduction don't worry much. Kitchen takes time best believe in the end product*/    
+}
