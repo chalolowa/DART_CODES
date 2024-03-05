132737bd3a19ce0e3da4cee9880c151c56838b8b
chalolowa <wangwelocha@students.uonbi.ac.ke>
2024-03-05 21:05:38 +0300

    Changes to be committed: 	modified:   main.dart

--- main.dart
+++ main.dart
@@ -2,6 +2,13 @@ void main() {
   String name = "locha";
   int age = 1000;
   bool isSingle = true;
+  var fruits = ['apple', 'banana', 'orange'];
+  var numbers = List<int>.filled(3, 0);
+  Map<String, int> studentGrades = {
+    'Alice': 90,
+    'Bob': 85,
+    'Charlie': 95,
+  };
 
   //Don't mind this just thinking in between
 
@@ -10,6 +17,9 @@ void main() {
   print(isSingle);
   print(
       "Hello there!! My name is $name and i'm $int years old. Also very $isSingle");
-      
-  /*Oh! its been fun bit short but its introduction don't worry much. Kitchen takes time best believe in the end product*/    
+  print(fruits);
+  print(numbers);
+  print(studentGrades);
+  
+  /*Oh! its been fun bit short but its introduction don't worry much. Kitchen takes time best believe in the end product*/
 }
